ae892caa8751581e67d02fd0f4b4b0c1d5b67468
Mkisecok <mahmutkisecok@gmail.com>
2021-12-13 08:30:14 +0100

    Create dom elements

---
+++ Reviews/app.js
@@ -0,0 +1,25 @@
+import img1 from './images/img1.jpg'
+import img2 from './images/img2.jpg'
+import img3 from './images/img3.jpg'
+const reviews = [
+    {
+    id: 1,
+    name: 'susan smith',
+    job: 'web developer',
+    img: './images/img1.jpg',
+    text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab consectetur omnis architecto voluptatem! Tempore eius inventore dolorem, itaque voluptatem facilis voluptatum. Ad doloribus, perspiciatis molestias minus unde culpa quam perferendis!'
+    },
+    {
+    id: 2,
+    name: 'john doe',
+    job: 'designer',
+    img: './images/img2.jpg',
+    text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab consectetur omnis architecto voluptatem! Tempore eius inventore dolorem, itaque voluptatem facilis voluptatum. Ad doloribus, perspiciatis molestias minus unde culpa quam perferendis!'
+    },
+    {
+    id: 1,
+    name: 'max mustermann',
+    job: 'data scientitst',
+    img: './images/img3.jpg',
+    text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab consectetur omnis architecto' },
+]
