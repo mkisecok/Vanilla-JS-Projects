64df2ca398db381d030b1dfe5dd36270c2f681ca
Mkisecok <mahmutkisecok@gmail.com>
2021-12-14 08:14:41 +0100

    Add show person func and randombtn

--- Reviews/app.js
+++ Reviews/app.js
@@ -39,17 +39,41 @@ let currentItem=0;
 
 //load intial item
 window.addEventListener('DOMContentLoaded', function(){
-    showPerson(currentItem)
+    showPerson()
    
 });
 
 //show person based on item
 function showPerson(person){
     
-    const item=reviews[person];
-    console.log(img);
+    const item=reviews[currentItem];
     img.src= item.img;
     author.textContent=item.name;
     job.textContent=item.job;
     info.textContent=item.text;
 }
+
+//show next person
+nextBtn.addEventListener('click', ()=>{
+    currentItem++;
+    if(currentItem > reviews.length - 1 ){
+        currentItem=0;
+    }
+    showPerson()
+   
+});
+
+//show prev person 
+prevBtn.addEventListener('click', ()=>{
+    currentItem--;
+    if(currentItem < 0){
+        currentItem=reviews.length-1;
+    }
+    showPerson()
+});
+
+// show random person   
+randomBtn.addEventListener('click', ()=>{
+    currentItem=Math.floor(Math.random()*reviews.length);
+    showPerson()
+})
